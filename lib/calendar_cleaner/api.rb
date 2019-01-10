9b1498afbdb2ca7ac0cb7a2f50a7dadd9a4b231e
Stuart Olivera <stuart@stuartolivera.com>
2019-01-09 20:00:33 -0500

    Update api.rb for newest Google API docs

--- lib/calendar_cleaner/api.rb
+++ lib/calendar_cleaner/api.rb
@@ -8,8 +8,11 @@ require 'fileutils'
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 APPLICATION_NAME = 'Calendar Cleaner'
-CLIENT_SECRETS_PATH = 'client_secret.json'
-CREDENTIALS_PATH = File.join(Dir.home, '.credentials', 'calendar-ruby-quickstart.yaml')
+CREDENTIALS_PATH = 'credentials.json'
+# The file token.yaml stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+TOKEN_PATH = 'token.yaml'
 SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
 
 module CalendarCleaner
@@ -40,22 +43,15 @@ module CalendarCleaner
     #
     # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
     def authorize
-      FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
-
-      client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
-      token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
-      authorizer = Google::Auth::UserAuthorizer.new(
-        client_id, SCOPE, token_store
-      )
+      client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
+      token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
+      authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
       user_id = 'default'
       credentials = authorizer.get_credentials(user_id)
       if credentials.nil?
-        url = authorizer.get_authorization_url(
-          base_url: OOB_URI
-        )
+        url = authorizer.get_authorization_url(base_url: OOB_URI)
         puts 'Open the following URL in the browser and enter the ' \
-             'resulting code after authorization'
-        puts url
+            "resulting code after authorization:\n" + url
         code = gets
         credentials = authorizer.get_and_store_credentials_from_code(
           user_id: user_id, code: code, base_url: OOB_URI
